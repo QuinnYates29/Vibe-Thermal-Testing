0372d59520fc8b50a06f24f20114877d2c216950
quinnyates <quinnyates3229@gmail.com>
2025-09-25 21:04:54 -0400

    Added initial setup with everything from STM32 Template

---
+++ scripts/flash.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Load the project name from the Makefile
+PROJECT_NAME=template
+PROJECT_VERSION=f33
+if [[ $(cat Makefile) =~ PROJECT_NAME[[:space:]]*:=[[:space:]]*([^[:space:]]*) ]]; then 
+    PROJECT_NAME=${BASH_REMATCH[1]}
+fi
+if [[ $(cat Makefile) =~ PROJECT_VERSION[[:space:]]*:=[[:space:]]*([^[:space:]]*) ]]; then 
+    PROJECT_VERSION=${BASH_REMATCH[1]}
+fi
+
+ELF=build/stm32/$PROJECT_NAME-$PROJECT_VERSION.elf
+
+echo "Programming with $ELF"
+
+openocd -f ./openocd.cfg -c "program ${ELF} verify reset" -c "exit"
